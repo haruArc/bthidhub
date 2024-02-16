a7e83a55b9380f986d21c68d74681c7ae07f76ea
haru_arc <hiroaki.willow@gmail.com>
2024-02-16 18:35:33 +0900

    Add switching host feature to Default.

--- hid_message_filter.py
+++ hid_message_filter.py
@@ -4,6 +4,10 @@ from typing import Optional
 
 class HIDMessageFilter:
     def filter_message_to_host(self, msg: bytes) -> Optional[bytes]:
+        if msg == b'\x01\x05\x00\x2b\x00\x00\x00\x00\x00' or msg == b'\x05\x00\x2b\x00\x00\x00\x00\x00':
+            print("host switch")
+            return b'\xff'
+
         if len(msg) == 8:
             return b'\xa1\x01' + msg
 
